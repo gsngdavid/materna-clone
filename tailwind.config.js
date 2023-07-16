70dab667a12abb259a18515c74893a51f96317e4
gsngdavid <gsngdavid@gmail.com>
2023-07-16 07:25:56 +0200

    feat: navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "red-primary": "#c30a17"
+      },
+      backgroundImage: {
+        "hero": "url('../images/hero-bg.svg')"
       }
     },
   },
