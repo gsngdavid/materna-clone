a99a097a3fb441f9a3a4bbd5cbe391112c1c86f1
gsngdavid <gsngdavid@gmail.com>
2023-07-16 06:51:08 +0200

    feat: header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "red-primary": "#c30a17"
+      }
+    },
+  },
+  plugins: [],
+}
+
